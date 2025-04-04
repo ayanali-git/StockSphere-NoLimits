644247ede0a90271b614dccc3fe41c6f3aeee0e5
ayan-x1 <pathanayan8347@gmail.com>
2025-04-04 10:42:52 +0530

    Updated AuthProvider.tsx with new changes

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -1,4 +1,3 @@
-// src/context/AuthProvider.tsx
 import { createContext, useContext, useEffect, useState } from "react";
 import { auth } from "../lib/firebase";
 import { onAuthStateChanged, User } from "firebase/auth";
